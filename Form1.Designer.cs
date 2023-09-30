8738e76b7b60b86bfcff845b7d1b15ea9388affc
berkeyucetas <berkeeyucetass@gmail.com>
2023-09-30 14:41:24 +0300

    Vücut Kütle Endeksi Bir Günde Bir Proje #6
    Vücut Ağırlığı (kg.) / Boy uzunluğunun karesi (m2.)
    formülünü kullanarak vücut kitle endeksini bulan ve text2 ye ideal kilonun altında veya ideal kilonun çok üstünde uyarıları veriyor.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Kilo_Endexsi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtboy = new System.Windows.Forms.TextBox();
+            this.txtkilo = new System.Windows.Forms.TextBox();
+            this.txtyas = new System.Windows.Forms.TextBox();
+            this.cmbcinsiyet = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtendeks = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Boy:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(13, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kilo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(13, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yaş:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(13, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cinsiyet:";
+            // 
+            // txtboy
+            // 
+            this.txtboy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtboy.Location = new System.Drawing.Point(134, 17);
+            this.txtboy.Name = "txtboy";
+            this.txtboy.Size = new System.Drawing.Size(197, 36);
+            this.txtboy.TabIndex = 4;
+            // 
+            // txtkilo
+            // 
+            this.txtkilo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtkilo.Location = new System.Drawing.Point(134, 68);
+            this.txtkilo.Name = "txtkilo";
+            this.txtkilo.Size = new System.Drawing.Size(197, 36);
+            this.txtkilo.TabIndex = 5;
+            // 
+            // txtyas
+            // 
+            this.txtyas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtyas.Location = new System.Drawing.Point(134, 118);
+            this.txtyas.Name = "txtyas";
+            this.txtyas.Size = new System.Drawing.Size(197, 36);
+            this.txtyas.TabIndex = 6;
+            // 
+            // cmbcinsiyet
+            // 
+            this.cmbcinsiyet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbcinsiyet.FormattingEnabled = true;
+            this.cmbcinsiyet.Items.AddRange(new object[] {
+            "Man",
+            "Women"});
+            this.cmbcinsiyet.Location = new System.Drawing.Point(153, 176);
+            this.cmbcinsiyet.Name = "cmbcinsiyet";
+            this.cmbcinsiyet.Size = new System.Drawing.Size(178, 37);
+            this.cmbcinsiyet.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(18, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 51);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtendeks
+            // 
+            this.txtendeks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtendeks.Location = new System.Drawing.Point(18, 321);
+            this.txtendeks.Multiline = true;
+            this.txtendeks.Name = "txtendeks";
+            this.txtendeks.Size = new System.Drawing.Size(280, 116);
+            this.txtendeks.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.RosyBrown;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(304, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 116);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(382, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtendeks);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbcinsiyet);
+            this.Controls.Add(this.txtyas);
+            this.Controls.Add(this.txtkilo);
+            this.Controls.Add(this.txtboy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Vücut Kitle Endeksi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtboy;
+        private System.Windows.Forms.TextBox txtkilo;
+        private System.Windows.Forms.TextBox txtyas;
+        private System.Windows.Forms.ComboBox cmbcinsiyet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtendeks;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Kilo Endexsi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kilo Endexsi")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("171896db-a485-4f48-a429-56e456fe7b08")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
